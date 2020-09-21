6644eb6f3ac6ade91fcca51cc2f3f7b575a6f8c0
JiangBowen0008 <jbw0008@outlook.com>
2020-09-21 18:28:28 +0800

    tested to run with custom dataset

--- generateImages.sh
+++ generateImages.sh
@@ -1,7 +1,7 @@
 for i in $(seq 1 $1)
 do
-    randport=$(python3 -S -c "import random; print(random.randrange(1,15))")
+    randport1=$(python3 -S -c "import random; print(random.randrange(1,15))")
     echo $randport
-    python3 run.py ImageGenConfig/config.yaml BOP lmo ../bop_toolkit resources/cctextures ../output $randport
+    python3 run.py ImageGenConfig/config.yaml BOP custom-grasp $randport1 custom-suction $randport1  ../bop_toolkit resources/cctextures ../output 
 done
 python3 ../bop_toolkit/scripts/calc_gt_masks.py
