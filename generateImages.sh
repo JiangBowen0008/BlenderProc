c97dc17e4e9ca00add636839a6c4bebfa58e2c7b
JiangBowen0008 <jbw0008@outlook.com>
2020-10-16 20:03:52 +0800

    test set generation command added

--- generateImages.sh
+++ generateImages.sh
@@ -4,5 +4,6 @@ do
     randport1=$(python3 -S -c "import random; print(random.randrange(1,15))")
     echo $randport
     python3 run.py ImageGenConfig/config.yaml BOP custom-grasp $randport1 custom-suction $randport1  ../bop_toolkit resources/cctextures ../output 
+    # python3 run.py ImageGenConfig/config.yaml BOP_test custom-grasp $randport1 custom-suction $randport1  ../bop_toolkit resources/cctextures ../output 
 done
 python3 ../bop_toolkit/scripts/calc_gt_masks.py
